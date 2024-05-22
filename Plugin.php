887cbac9ac8e378d19383b7519edf25121955a53
Martin Šimko <simko@galton.sk>
2024-05-22 10:27:39 +0200

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,101 @@
+<?php namespace Mcore\ClientMonitor;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Plugin Information File
+ *
+ * @link https://docs.octobercms.com/3.x/extend/system/plugins.html
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * pluginDetails about this plugin.
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'ClientMonitor',
+            'description' => 'No description provided yet...',
+            'author' => 'Mcore',
+            'icon' => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * register method, called when the plugin is first registered.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * boot method, called right before the request route.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * registerComponents used by the frontend.
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Mcore\ClientMonitor\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * registerPermissions used by the backend.
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'mcore.clientmonitor.some_permission' => [
+                'tab' => 'ClientMonitor',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    public function registerSettings()
+    {
+        return [
+            'clientmonitor' => [
+                'label' => 'Client monitor setting',
+                'description' => 'Setting....',
+                'category' => 'CATEGORY_CMS',
+                'icon' => 'octo-icon-user-group',
+                'class' => \Mcore\ClientMonitor\Models\MonitorSetting::class,
+                'size' => 'medium',
+                'order' => -100,
+            ]
+        ];
+    }
+
+    /**
+     * registerNavigation used by the backend.
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'clientmonitor' => [
+                'label' => 'ClientMonitor',
+                'url' => Backend::url('mcore/clientmonitor/mycontroller'),
+                'icon' => 'icon-leaf',
+                'permissions' => ['mcore.clientmonitor.*'],
+                'order' => 500,
+            ],
+        ];
+    }
+}
---
+++ routes.php
@@ -0,0 +1,57 @@
+<?php
+use System\Classes\UpdateManager;
+use System\Models\PluginVersion;
+use Mcore\ClientMonitor\Models\MonitorSetting;
+use Illuminate\Http\Request;
+
+
+Route::post('/monitor/client/info', function(Request $request) {
+	
+	$responseJson = [];
+	$api_token = post('api_token');
+	$api_key = MonitorSetting::get('api_key');
+	
+	$domain = $request->getScheme().'://'.$request->getHost();    
+    
+    if($api_token === $api_key) {
+    	
+	    $projectVersion = UpdateManager::instance()->getCurrentVersion();
+	    $pluginVersionList = PluginVersion::get()
+	    ->keyBy('code')
+	    ->map(function ($record) {
+	        return [
+	            'code' => $record->code,
+	            'name' => $record->name,
+	            'description' => $record->description,
+	            'author' => $record->author,
+	            'version' => $record->version,
+	            'is_frozen' => $record->is_frozen,
+	            'is_disabled' => $record->is_disabled,
+	        ];
+	    })
+	    ->toArray();
+
+	    $responseJson = [
+	    	'domain' => $domain,
+	    	'version' => $projectVersion,
+	    	'plugins' => $pluginVersionList,
+	    ];
+    }
+
+    return response()->json($responseJson);
+});
+
+
+Route::post('/monitor/client/ping', function() {
+    
+    $statusCode = 400;
+    $api_token = post('api_token');
+    $api_key = MonitorSetting::get('api_key');
+
+    if($api_token === $api_key) {	
+    	$statusCode = 201;
+	    return response(['statusCode' => $statusCode, 'message' => $api_key], $statusCode);
+    }
+	
+    return response(['statusCode' => $statusCode, 'message' => 'Wrong Credintals'], $statusCode);
+});
